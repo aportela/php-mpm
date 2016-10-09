e669290c6648afa0e6f54ac74b1bfce37cd24a03
alex <766f6964+github@gmail.com>
2016-10-09 18:37:32 +0200

    new group api method (add)

---
+++ api/group/add.php
@@ -0,0 +1,67 @@
+<?php
+    /**
+    *   /api/group/add.php
+    *   description: add new group
+    *
+    *   request method: POST
+    *
+    *   @param string id 
+    *   @param string name
+    *   @param string description
+    *   @param array users
+    */
+    namespace PHP_MPM;
+
+    require_once "../../include/configuration.php";
+    require_once "../../include/class.User.php";
+    require_once "../../include/class.Group.php";
+    require_once "../../include/class.CustomExceptions.php";
+    require_once "../../include/class.Error.php";
+
+    ob_start();    
+    $result = array("success" => false);
+    try {
+        $g = new Group();         
+        $u->set(
+            isset($_POST["id"]) ? $_POST["id"]: "", 
+            isset($_POST["name"]) ? $_POST["name"]: "", 
+            isset($_POST["description"]) ? $_POST["description"]: "",
+            array()
+        );
+        $g->add();
+        $result["success"] = true;
+        ob_clean();
+    } catch (MPMInvalidParamsException $e) {
+        Error::save($e);
+        ob_clean();
+        header("HTTP/1.1 400 Bad Request", 400, true);
+        if (ENVIRONMENT_DEV && DEBUG) {
+            $result["exception"] = print_r($e, true);
+        }
+    } catch (MPMAlreadyExistsException $e) {
+        Error::save($e);
+        ob_clean();
+        header("HTTP/1.1 409 Conflict", 409, true);
+        if (ENVIRONMENT_DEV && DEBUG) {
+            $result["exception"] = print_r($e, true);
+        }
+    } catch (\PDOException $e) {
+        Error::save($e);
+        ob_clean();
+        header("HTTP/1.1 500 Internal Server Error", 500, true);        
+        if (ENVIRONMENT_DEV && DEBUG) {
+            $result["exception"] = print_r($e, true);
+        }        
+    } catch (\Throwable $e) {
+        Error::save($e);
+        ob_clean();
+        header("HTTP/1.1 500 Internal Server Error", 500, true);        
+        if (ENVIRONMENT_DEV && DEBUG) {
+            $result["exception"] = print_r($e, true);
+        }
+    } finally {
+        header("Content-Type: application/json; charset=utf-8");
+        echo json_encode($result);
+        ob_end_flush();
+    }
+?>
