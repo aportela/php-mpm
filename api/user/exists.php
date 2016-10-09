9f755ba4c461901a33c3bfa05f20236e9c4981a5
alex <766f6964+github@gmail.com>
2016-10-09 11:34:19 +0200

    new user api method

---
+++ api/user/exists.php
@@ -0,0 +1,46 @@
+<?php
+    /**
+    *   /api/user/exists.php
+    *   description: check user (email) existence
+    *
+    *   request method: POST
+    *
+    *   @param string email 
+    */
+    namespace PHP_MPM;
+
+    require_once "../../include/configuration.php";
+    require_once "../../include/class.User.php";
+    require_once "../../include/class.CustomExceptions.php";
+
+    ob_start();    
+    $result = array("success" => false, "exists" => false);
+    try {
+        $u = new User();         
+        $u->set("", isset($_POST["email"]) ? $_POST["email"]: "", "");
+        $result["exists"] = $u->exists();
+        ob_clean();
+        if ($result["exists"]) {
+            header("HTTP/1.1 200 OK", 200, true);
+        } else {
+            header("HTTP/1.0 404 Not Found", 404, true);
+        }
+        $result["success"] = true;
+    } catch (MPMInvalidParamsException $e) {
+        ob_clean();
+        header("HTTP/1.1 400 Bad Request", 400, true);
+        if (ENVIRONMENT_DEV && DEBUG) {
+            $result["exception"] = print_r($e, true);
+        }
+    } catch (\Throwable $e) {
+        ob_clean();
+        header("HTTP/1.1 500 Internal Server Error", 500, true);        
+        if (ENVIRONMENT_DEV && DEBUG) {
+            $result["exception"] = print_r($e, true);
+        }
+    } finally {
+        header("Content-Type: application/json; charset=utf-8");
+        echo json_encode($result);
+        ob_end_flush();
+    }
+?>
