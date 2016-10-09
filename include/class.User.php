7d4ca58c9e5795eaf77dfa906a35e76f2097d982
alex <766f6964+github@gmail.com>
2016-10-09 16:00:46 +0200

    add method for user class

--- include/class.User.php
+++ include/class.User.php
@@ -3,6 +3,7 @@
 
     require_once "class.CustomExceptions.php";
     require_once "class.Database.php";
+    require_once "class.Utils.php";
 
     /**
     *   user class
@@ -23,15 +24,46 @@
             $this->password = $password;
         }
 
+        /**
+        *   check user (by email) existence
+        */
         public function exists() {
-            if (empty($this->email)) {
-                throw new MPMInvalidParamsException(print_r($this));
+            if (empty($this->email)) {                
+                throw new MPMInvalidParamsException(print_r(get_object_vars($this), true));
             } else {
                 $param = new DatabaseParam();
                 $param->str(":email", $this->email);                
                 $rows = Database::execWithResult(" SELECT * FROM USER WHERE email = :email ", array($param));
                 return(count($rows) > 0);                
             }            
+        }
+
+        /**
+        *   add new user
+        */
+        public function add() {            
+            if ($this->exists()) {
+                throw new MPMAlreadyExistsException(print_r(get_object_vars($this), true));
+            } else {
+                if (empty($this->email) || empty($this->password)) {
+                    throw new MPMInvalidParamsException(print_r(get_object_vars($this), true));
+                } else {
+                    if (empty($this->id)) {
+                        $this->id = Utils::uuid();
+                    }
+                    $params = array();
+                    $param = new DatabaseParam();
+                    $param->str(":id", $this->id);
+                    $params[] = $param;                
+                    $param = new DatabaseParam();
+                    $param->str(":email", $this->email);
+                    $params[] = $param;                
+                    $param = new DatabaseParam();
+                    $param->str(":password", password_hash($this->password, PASSWORD_BCRYPT, array("cost" => 12)));
+                    $params[] = $param;                                
+                    Database::execWithoutResult(" INSERT INTO USER (id, email, password, created) VALUES (:id, :email, :password, CURRENT_TIMESTAMP) ", $params);
+                }
+            }
         }        
     }
 ?>
