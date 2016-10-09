99009b8d416228e200eae1e78a13bd52e4703666
alex <766f6964+github@gmail.com>
2016-10-09 15:59:53 +0200

    new custom exception

--- include/class.CustomExceptions.php
+++ include/class.CustomExceptions.php
@@ -14,4 +14,17 @@
         }
     }
 
+    /**
+    *   already exists exception
+    */
+    class MPMAlreadyExistsException extends \Exception {
+        public function __construct($message = "", $code = 0, Exception $previous = null) {
+            parent::__construct($message, $code, $previous);
+        }
+
+        public function __toString() {
+            return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
+        }
+    }
+
 ?>
