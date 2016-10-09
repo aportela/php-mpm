47921ca6f8096266c1053548c6be5e2a3d7559f5
alex <766f6964+github@gmail.com>
2016-10-09 16:08:48 +0200

    new custom exception

--- include/class.CustomExceptions.php
+++ include/class.CustomExceptions.php
@@ -27,4 +27,17 @@
         }
     }
 
+    /**
+    *   not found exception
+    */
+    class MPMNotFoundException extends \Exception {
+        public function __construct($message = "", $code = 0, Exception $previous = null) {
+            parent::__construct($message, $code, $previous);
+        }
+
+        public function __toString() {
+            return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
+        }
+    }
+
 ?>
