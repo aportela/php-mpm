af7bb5e650077540a3d561dff79259014d3ad361
alex <766f6964+github@gmail.com>
2016-10-09 18:27:05 +0200

    new custom exception

--- include/class.CustomExceptions.php
+++ include/class.CustomExceptions.php
@@ -41,6 +41,19 @@
     }
 
     /**
+    *   authenticated session required exception
+    */
+    class MPMAuthSessionRequiredException extends \Exception {
+        public function __construct($message = "", $code = 0, Exception $previous = null) {
+            parent::__construct($message, $code, $previous);
+        }
+
+        public function __toString() {
+            return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
+        }
+    }
+
+    /**
     *   administration privileges required exception
     */
     class MPMAdminPrivilegesRequiredException extends \Exception {
