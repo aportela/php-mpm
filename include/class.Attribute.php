964a3dd1b601d1039f6065d26c96cb8b88f7c4d4
alex <766f6964+github@gmail.com>
2016-10-09 23:19:56 +0200

    new class for manage element attributes

---
+++ include/class.Attribute.php
@@ -0,0 +1,38 @@
+<?php
+    namespace PHP_MPM;
+
+    /**
+    *   attribute type definitions
+    */
+    abstract class AttributeType {
+        const NONE = 0;             // default (_DON'T USE THIS_)
+        const TEXT_SHORT = 1;       // short string (0-255 chars)
+        const TEXT_LONG = 2;        // long string (memo)
+        const NUMBER_INTEGER = 3;   // integer number
+        const NUMBER_DECIMAL = 4;   // decimal number
+        const DATE = 5;             // date 
+        const TIME = 6;             // time
+        const DATETIME = 7;         // date & time
+    }
+
+    /**
+    *   attribute class
+    */
+    class Attribute {
+        private $id;
+        private $name;
+        private $description;
+        private $type;
+
+		public function __construct () { }
+
+        public function __destruct() { }
+
+        public function set(string $id = "", string $name = "", string $description = "", int $type = AttributeType::NONE) {
+            $this->id = $id;
+            $this->name = $name;
+            $this->description = $description;
+            $this->type = $type;            
+        }                
+    }
+?>
