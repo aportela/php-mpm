609577ae27885fa53e0ec023985cb01c18bef733
alex <766f6964+github@gmail.com>
2016-10-09 16:00:16 +0200

    catch for PDOException

--- api/user/exists.php
+++ api/user/exists.php
@@ -32,6 +32,12 @@
         if (ENVIRONMENT_DEV && DEBUG) {
             $result["exception"] = print_r($e, true);
         }
+    } catch (\PDOException $e) {
+        ob_clean();
+        header("HTTP/1.1 500 Internal Server Error", 500, true);        
+        if (ENVIRONMENT_DEV && DEBUG) {
+            $result["exception"] = print_r($e, true);
+        }    
     } catch (\Throwable $e) {
         ob_clean();
         header("HTTP/1.1 500 Internal Server Error", 500, true);        
