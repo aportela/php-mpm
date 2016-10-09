49b83d4946ea69a743fe5b03fa4394e489ef8339
alex <766f6964+github@gmail.com>
2016-10-09 16:23:42 +0200

    new user class method for sign out

--- include/class.User.php
+++ include/class.User.php
@@ -103,5 +103,19 @@
                 return(true);
             }
         }
+
+        /**
+        *   sign out
+        *
+        *   (Frxstrem) http://stackoverflow.com/a/3512570
+        */
+        public function logout() {
+            $_SESSION = array();
+            if (ini_get("session.use_cookies")) {
+                $params = session_get_cookie_params();
+                setcookie(session_name(), '', time() - 42000, $params["path"], $params["domain"], $params["secure"], $params["httponly"]);
+            }
+            session_destroy();
+        }
     }
 ?>
