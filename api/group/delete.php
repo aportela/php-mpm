b894c06ddefb8838d8abada561c874f0b28a3faa
alex <766f6964+github@gmail.com>
2016-10-10 20:56:49 +0200

    new group api method for delete

---
+++ api/group/delete.php
@@ -0,0 +1,78 @@
+<?php
+    /**
+    *   /api/group/delete.php
+    *   description: delete group
+    *
+    *   request method: POST
+    *
+    *   @param string id 
+    */
+    namespace PHP_MPM;
+
+    require_once "../../include/configuration.php";
+    require_once "../../include/class.User.php";
+    require_once "../../include/class.Group.php";
+    require_once "../../include/class.CustomExceptions.php";
+    require_once "../../include/class.Error.php";
+
+    ob_start();    
+    $result = array("success" => false);
+    try {
+        $g = new Attribute();         
+        $g->set(
+            isset($_POST["id"]) ? $_POST["id"]: "", 
+            "", 
+            "",
+            array()
+        );
+        $g->delete();
+        $result["success"] = true;
+        ob_clean();
+    } catch (MPMInvalidParamsException $e) {
+        Error::save($e);
+        ob_clean();
+        header("HTTP/1.1 400 Bad Request", 400, true);
+        if (ENVIRONMENT_DEV && DEBUG) {
+            $result["exception"] = print_r($e, true);
+        }
+    } catch (MPMNotFoundException $e) {
+        Error::save($e);
+        ob_clean();
+        header("HTTP/1.0 404 Not Found", 404, true);
+        if (ENVIRONMENT_DEV && DEBUG) {
+            $result["exception"] = print_r($e, true);
+        }
+    } catch (MPMAuthSessionRequiredException $e) {
+        Error::save($e);
+        ob_clean();
+        header("HTTP/1.0 403 Forbidden", 403, true);
+        if (ENVIRONMENT_DEV && DEBUG) {
+            $result["exception"] = print_r($e, true);
+        }        
+    } catch (MPMAdminPrivilegesRequiredException $e) {
+        Error::save($e);
+        ob_clean();
+        header("HTTP/1.0 403 Forbidden", 403, true);
+        if (ENVIRONMENT_DEV && DEBUG) {
+            $result["exception"] = print_r($e, true);
+        }        
+    } catch (\PDOException $e) {
+        Error::save($e);
+        ob_clean();
+        header("HTTP/1.1 500 Internal Server Error", 500, true);        
+        if (ENVIRONMENT_DEV && DEBUG) {
+            $result["exception"] = print_r($e, true);
+        }        
+    } catch (\Throwable $e) {
+        Error::save($e);
+        ob_clean();
+        header("HTTP/1.1 500 Internal Server Error", 500, true);        
+        if (ENVIRONMENT_DEV && DEBUG) {
+            $result["exception"] = print_r($e, true);
+        }
+    } finally {
+        header("Content-Type: application/json; charset=utf-8");
+        echo json_encode($result);
+        ob_end_flush();
+    }
+?>
