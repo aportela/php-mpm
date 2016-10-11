01da70b17861411ec4bb8dac8222a5659d35274e
alex <766f6964+github@gmail.com>
2016-10-11 20:38:15 +0200

    more user tests

--- include/tests/UserTest.php
+++ include/tests/UserTest.php
@@ -5,6 +5,9 @@
     require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . "class.Utils.php";
     require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . "class.CustomExceptions.php";
 
+    ob_start();
+    session_start();
+
     class UserTest extends \PHPUnit_Framework_TestCase {
 
         public function testExistsWithExistentEmail() {
@@ -55,7 +58,65 @@
             } finally {
                 $this->assertNull($err);
             }            
+        }
+
+        public function testLoginWithEmptyIdAndEmail() {
+            $this->setExpectedException('PHP_MPM\MPMInvalidParamsException');
+            $u = new User();
+            $u->login();                        
         }        
+
+        public function testLoginWithNotExistentId() {
+            $this->setExpectedException('PHP_MPM\MPMNotFoundException');
+            $u = new User();
+            $u->set("z-z-z-z-z-z-z-z", "", "", 0);
+            $u->login();
+        }        
+
+        public function testLoginWithNotExistenEmail() {
+            $this->setExpectedException('PHP_MPM\MPMNotFoundException');
+            $u = new User();
+            $u->set("", "thisemaildonotexists@server.com", "", 0);
+            $u->login();                        
+        }        
+
+        public function testLoginWithExistentIdAndInvalidPassword() {
+            $u = new User();
+            $u->set("00000000-0000-0000-0000-000000000000", "", "WRONG_PASSWORD", 0);
+            $this->assertFalse($u->login());
+        }        
+
+        public function testLoginWithExistenEmailAndInvalidPassword() {
+            $u = new User();
+            $u->set("", "admin@localhost", "WRONG_PASSWORD", 0);
+            $this->assertFalse($u->login());                        
+        }        
+
+        public function testLoginWithExistentIdAndValidPassword() {
+            $u = new User();
+            $u->set("00000000-0000-0000-0000-000000000000", "", "password", 0);
+            $this->assertTrue($u->login());
+        }        
+
+        public function testLoginWithExistenEmailAndValidPassword() {
+            $u = new User();
+            $u->set("", "admin@localhost", "password", 0);
+            $this->assertTrue($u->login());                        
+        }        
+
+        public function testIsAuthenticatedWithAuthSession() {
+            $u = new User();
+            $u->set("00000000-0000-0000-0000-000000000000", "", "password", 0);
+            $u->login();
+            $this->assertTrue(User::isAuthenticated());
+        }
+
+        public function testIsAuthenticatedWithoutAuthSession() {
+            $u = new User();
+            $u->logout();
+            $this->assertFalse(User::isAuthenticated());
+        }
+
     }
 
 ?>
