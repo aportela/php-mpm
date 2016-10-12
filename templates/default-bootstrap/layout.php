19803a4f61263b53f3f8556632087c76f5b9d6c8
alex <766f6964+github@gmail.com>
2016-10-12 12:00:52 +0200

    added animated icon on background ajax calls

--- templates/default-bootstrap/layout.php
+++ templates/default-bootstrap/layout.php
@@ -2,6 +2,7 @@
 <html lang="en">
   <?php include "html-head.php"; ?>
   <body>
+  <i id="ajax_icon" class="fa fa-cog fa-spin fa-1x fa-fw"></i>
     <?php
       if ($_TEMPLATE["is_logged"] == false) {
           include "signin.php";
--- templates/default-bootstrap/static/scripts/script.js
+++ templates/default-bootstrap/static/scripts/script.js
@@ -1,3 +1,20 @@
+/**
+ * show top right static loading icon while xhr (ajax) events
+ * (John Culviner) http://stackoverflow.com/a/27363569
+ */
+var $loading = $('i#ajax_icon').hide();
+
+(function() {
+    var origOpen = XMLHttpRequest.prototype.open;
+    XMLHttpRequest.prototype.open = function() {
+        $loading.show();
+        this.addEventListener('load', function() {
+            $loading.hide();
+        });
+        origOpen.apply(this, arguments);
+    };
+})();
+
 $.fn.clearValidationMessages = function() {
     $("div.form-group").removeClass("has-danger").removeClass("has-warning");
     $("div.form-group .form-control-danger").removeClass("form-control-danger").removeClass("form-control-warning");
