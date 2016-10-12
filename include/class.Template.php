6c6310949d2da1fda6e101b256bbebca4877f9b9
alex <766f6964+github@gmail.com>
2016-10-12 10:47:04 +0200

    new "temporal templating engine" for testing

---
+++ include/class.Template.php
@@ -0,0 +1,36 @@
+<?php
+    namespace PHP_MPM;
+
+    require_once __DIR__ . DIRECTORY_SEPARATOR . "configuration.php";
+    require_once __DIR__ . DIRECTORY_SEPARATOR . "class.CustomExceptions.php";
+
+    /**
+    *   simple (temporal ?) templating class
+    */
+    class Template {
+        private $path;
+        public $name;
+
+		public function __construct (string $name = "") {
+            $this->path = TEMPLATES_PATH . DIRECTORY_SEPARATOR . DEFAULT_TEMPLATE_THEME . DIRECTORY_SEPARATOR . $name;             
+            if (! file_exists($this->path)) {
+                throw new MPMCustomTemplatingException("template not found on path " . $this->path);
+            } else {
+                $this->name = $name;
+            }
+        }
+
+        public function __destruct() { }
+
+        public function render($params) {
+            global $_TEMPLATE;
+            if (! isset($_TEMPLATE)) {
+                $_TEMPLATE = array();
+            }            
+            foreach ($params as $key => $value) {
+                $_TEMPLATE[$key] = $value;
+            }
+            include($this->path);
+        }        
+    }
+?>
