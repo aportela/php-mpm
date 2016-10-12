799f2600df52eb199cfae78df1c4d16574f1331d
alex <766f6964+github@gmail.com>
2016-10-12 16:48:08 +0200

    new tests for attribute class

--- include/tests/AttributeTest.php
+++ include/tests/AttributeTest.php
@@ -10,5 +10,99 @@
 
     class AttributeTest extends \PHPUnit_Framework_TestCase {
 
+        public function testExistsWithoutAuthSession() {
+            $this->setExpectedException('PHP_MPM\MPMAuthSessionRequiredException');
+            if (session_status() == PHP_SESSION_NONE) {
+                session_start();
+            }
+            $a = new Attribute();
+            $a->exists();                    
+        }
+
+        public function testExistsWithoutAuthAdminSession() {
+            /*
+            // TODO: default (non admin) user
+            $this->setExpectedException('PHP_MPM\MPMAdminPrivilegesRequiredException');
+            if (session_status() == PHP_SESSION_NONE) {
+                session_start();
+            }
+            $u = new User();
+            // TODO: normal (non admin user)
+            $u->set("", "admin@localhost", "password", 0);
+            $u->login();            
+            $a = new Attribute();
+            $a->exists();                    
+            */                    
+        }
+
+        public function testExistsWithExistentId() {
+            if (session_status() == PHP_SESSION_NONE) {
+                session_start();
+            }
+            $u = new User();
+            $u->set("", "admin@localhost", "password", 0);
+            $u->login();
+            $a = new Attribute();
+            $a->set(
+                "1111111-1111-1111-0000-111111111111",
+                "",
+                "",
+                AttributeType::NONE
+            );
+            $this->assertTrue($a->exists());        
+        }
+
+        public function testExistsWithNotExistentId() {
+            if (session_status() == PHP_SESSION_NONE) {
+                session_start();
+            }
+            $u = new User();
+            $u->set("", "admin@localhost", "password", 0);
+            $u->login();
+            $a = new Attribute();
+            $a->set(
+                "z-z-z--z-z-z-z",
+                "",
+                "",
+                AttributeType::NONE
+            );
+            $this->assertFalse($a->exists());        
+        }
+
+        public function testExistsWithExistentName() {
+            if (session_status() == PHP_SESSION_NONE) {
+                session_start();
+            }
+            $u = new User();
+            $u->set("", "admin@localhost", "password", 0);
+            $u->login();
+            $a = new Attribute();
+            $a->set(
+                "",
+                "Age",
+                "",
+                AttributeType::NONE
+            );
+            $this->assertTrue($a->exists());        
+        }
+
+        public function testExistsWithNotExistentName() {
+            if (session_status() == PHP_SESSION_NONE) {
+                session_start();
+            }
+            $u = new User();
+            $u->set("", "admin@localhost", "password", 0);
+            $u->login();
+            $a = new Attribute();
+            $a->set(
+                "",
+                "this_atribute_Name_DO_NOT_exISt__--",
+                "",
+                AttributeType::NONE
+                );
+            $this->assertFalse($a->exists());       
+        }
+
+
     }
 ?>
