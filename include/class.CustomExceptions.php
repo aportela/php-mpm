fb801eb66ce6a9e6bf996186698539341a3d60f9
alex <766f6964+github@gmail.com>
2016-10-12 10:47:15 +0200

    new custom exception for template errors

--- include/class.CustomExceptions.php
+++ include/class.CustomExceptions.php
@@ -66,4 +66,18 @@
         }
     }
 
+    /**
+    *   common template (custom templating) exception
+    */
+    class MPMCustomTemplatingException extends \Exception {
+        public function __construct($message = "", $code = 0, Exception $previous = null) {
+            parent::__construct($message, $code, $previous);
+        }
+
+        public function __toString() {
+            return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
+        }
+    }
+    
+
 ?>
