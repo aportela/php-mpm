5b0b4d325fbd1be3e3bd2cc70d509579cbeb8a7c
alex <766f6964+github@gmail.com>
2016-10-12 19:10:51 +0200

    user api method for register (normal, non admin) users

---
+++ api/user/signup.php
@@ -0,0 +1,65 @@
+<?php
+    /**
+    *   /api/user/signup.php
+    *   description: register new user
+    *
+    *   request method: POST
+    *
+    *   @param string id 
+    *   @param string email
+    *   @param string password
+    */
+    namespace PHP_MPM;
+
+    require_once "../../include/configuration.php";
+    require_once "../../include/class.User.php";
+    require_once "../../include/class.CustomExceptions.php";
+    require_once "../../include/class.Error.php";
+
+    ob_start();    
+    $result = array("success" => false);
+    try {
+        $u = new User();         
+        $u->set(
+            isset($_POST["id"]) ? $_POST["id"]: "", 
+            isset($_POST["email"]) ? $_POST["email"]: "", 
+            isset($_POST["password"]) ? $_POST["password"]: "",
+            UserType::DEFAULT
+        );
+        $u->signup();
+        $result["success"] = true;
+        ob_clean();
+    } catch (MPMInvalidParamsException $e) {
+        Error::save($e);
+        ob_clean();
+        header("HTTP/1.1 400 Bad Request", 400, true);
+        if (ENVIRONMENT_DEV && DEBUG) {
+            $result["exception"] = print_r($e, true);
+        }
+    } catch (MPMAlreadyExistsException $e) {
+        Error::save($e);
+        ob_clean();
+        header("HTTP/1.1 409 Conflict", 409, true);
+        if (ENVIRONMENT_DEV && DEBUG) {
+            $result["exception"] = print_r($e, true);
+        }
+    } catch (\PDOException $e) {
+        Error::save($e);
+        ob_clean();
+        header("HTTP/1.1 500 Internal Server Error", 500, true);        
+        if (ENVIRONMENT_DEV && DEBUG) {
+            $result["exception"] = print_r($e, true);
+        }        
+    } catch (\Throwable $e) {
+        Error::save($e);
+        ob_clean();
+        header("HTTP/1.1 500 Internal Server Error", 500, true);        
+        if (ENVIRONMENT_DEV && DEBUG) {
+            $result["exception"] = print_r($e, true);
+        }
+    } finally {
+        header("Content-Type: application/json; charset=utf-8");
+        echo json_encode($result);
+        ob_end_flush();
+    }
+?>
