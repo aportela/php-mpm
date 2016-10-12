25f07996d1ad9876f01612c01f00448207ab01cb
alex <766f6964+github@gmail.com>
2016-10-12 09:58:19 +0200

    new (empty) attribute tests

---
+++ include/tests/AttributeTest.php
@@ -0,0 +1,14 @@
+<?php
+    namespace PHP_MPM;
+
+    require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . "class.User.php";
+    require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . "class.Attribute.php";
+    require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . "class.Utils.php";
+    require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . "class.CustomExceptions.php";
+
+    ob_start();    
+
+    class AttributeTest extends \PHPUnit_Framework_TestCase {
+
+    }
+?>
