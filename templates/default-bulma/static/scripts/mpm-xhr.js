937bf332f1d1e39907bdc5c3a7a0a5de36431c9f
alex <766f6964+github@gmail.com>
2016-10-15 18:42:20 +0200

    added form functions for validations & enable/disable submit

--- templates/default-bulma/static/scripts/mpm-xhr.js
+++ templates/default-bulma/static/scripts/mpm-xhr.js
@@ -4,11 +4,57 @@ var mpm = mpm || {};
 
 mpm.form = mpm.form || {};
 
-mpm.form.submit = function (form, callback) {
+mpm.form.disableSubmit = function(form) {
+    $(form).find('button[type="submit"]').prop("disabled", true);
+};
+
+mpm.form.enableSubmit = function(form) {
+    $(form).find('button[type="submit"]').prop("disabled", false);
+};
+
+/**
+ * clear all form (input fields) validation messages
+ */
+mpm.form.clearValidationMessages = function(form) {
+    $(form).find("input.input").removeClass("is-danger").removeClass("is-warning");
+    $(form).find("span.help").remove();
+}
+
+/**
+ * put form (input field) validation warning message
+ */
+mpm.form.putValidationWarning = function(elementId, message) {
+    var element = $("p#" + elementId);
+    $(element).find("input.input").addClass("is-warning");
+    $(element).append('<span class="help is-warning">' + message + '</span>');
+}
+
+/**
+ * put form (input field) validation error message
+ */
+mpm.form.putValidationError = function(elementId, message) {
+    var element = $("p#" + elementId);
+    $(element).find("input.input").addClass("is-danger");
+    $(element).append('<span class="help is-danger">' + message + '</span>');
+}
+
+/**
+ * put form (input field) validation success message
+ */
+mpm.form.putValidationSuccess = function(elementId, message) {
+    var element = $("p#" + elementId);
+    $(element).append('<span class="help is-success">' + message + '</span>');
+}
+
+
+mpm.form.submit = function(form, callback) {
+    mpm.form.disableSubmit();
+    mpm.form.clearValidationMessages();
     var xhr = new XMLHttpRequest();
     xhr.open($(form).attr("method"), $(form).attr("action"), true);
-    xhr.onreadystatechange = function (e) {
+    xhr.onreadystatechange = function(e) {
         if (this.readyState == 4) {
+            mpm.form.enableSubmit();
             var result = null;
             try {
                 result = JSON.parse(xhr.responseText);
