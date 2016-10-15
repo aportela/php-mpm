9e065c4787f2eb4a33593f904276071cdeac8836
alex <766f6964+github@gmail.com>
2016-10-15 15:08:24 +0200

    added user (api) update method

---
+++ api/user/update.php
@@ -0,0 +1,84 @@
+<?php
+    /**
+    *   /api/user/add.php
+    *   description: add new user
+    *
+    *   request method: POST
+    *
+    *   @param string id 
+    *   @param string email
+    *   @param string password
+    *   @param int type
+    */
+    namespace PHP_MPM;
+
+    require_once "../../include/configuration.php";
+    require_once "../../include/class.User.php";
+    require_once "../../include/class.CustomExceptions.php";
+    require_once "../../include/class.Error.php";
+
+    ob_start();
+    
+    session_start();
+
+    $result = array("success" => false);
+    try {
+        $u = new \PHP_MPM\User();         
+        $u->set(
+            isset($_POST["id"]) ? $_POST["id"]: "", 
+            isset($_POST["email"]) ? $_POST["email"]: "", 
+            isset($_POST["password"]) ? $_POST["password"]: "",
+            isset($_POST["name"]) ? $_POST["name"]: "",
+            isset($_POST["type"]) ? $_POST["type"]: 0
+        );
+        $u->update();
+        $result["success"] = true;
+        ob_clean();
+    } catch (\PHP_MPM\MPMInvalidParamsException $e) {
+        Error::save($e);
+        ob_clean();
+        header("HTTP/1.1 400 Bad Request", 400, true);
+        if (ENVIRONMENT_DEV && DEBUG) {
+            $result["exception"] = print_r($e, true);
+        }
+    } catch (\PHP_MPM\MPMNotFoundException $e) {
+        Error::save($e);
+        ob_clean();
+        header("HTTP/1.0 404 Not Found", 404, true);
+        if (ENVIRONMENT_DEV && DEBUG) {
+            $result["exception"] = print_r($e, true);
+        }
+    } catch (\PHP_MPM\MPMAuthSessionRequiredException $e) {
+        Error::save($e);
+        ob_clean();
+        header("HTTP/1.0 403 Forbidden", 403, true);
+        if (ENVIRONMENT_DEV && DEBUG) {
+            $result["exception"] = print_r($e, true);
+        }        
+    } catch (\PHP_MPM\MPMAdminPrivilegesRequiredException $e) {
+        Error::save($e);
+        ob_clean();
+        header("HTTP/1.0 403 Forbidden", 403, true);
+        if (ENVIRONMENT_DEV && DEBUG) {
+            $result["exception"] = print_r($e, true);
+        }                
+    } catch (\PDOException $e) {
+        Error::save($e);
+        ob_clean();
+        header("HTTP/1.1 500 Internal Server Error", 500, true);        
+        if (ENVIRONMENT_DEV && DEBUG) {
+            $result["exception"] = print_r($e, true);
+        }        
+    } catch (\Throwable $e) {
+        Error::save($e);
+        ob_clean();
+        header("HTTP/1.1 500 Internal Server Error", 500, true);        
+        if (ENVIRONMENT_DEV && DEBUG) {
+            $result["exception"] = print_r($e, true);
+        }
+    } finally {
+        header("Content-Type: application/json; charset=utf-8");
+        echo json_encode($result);
+        ob_end_flush();
+    }
+?>
