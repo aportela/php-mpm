26577dc6adbe6efcb4cef5fdad477fc80d5b29cc
alex <766f6964+github@gmail.com>
2016-10-15 16:27:55 +0200

    new messages

--- templates/default-bulma/static/scripts/locale-en.js
+++ templates/default-bulma/static/scripts/locale-en.js
@@ -2,5 +2,12 @@
 
 const SIGN_IN_EMAIL_NOT_FOUND = "email not found";
 const SIGN_IN_INVALID_PASSWORD = "invalid password";
+const SIGN_IN_GENERAL_ERROR_MESSAGE = "error starting session";
 
 const SIGN_UP_EMAIL_EXISTS = "email already used";
+const SIGN_UP_GENERAL_ERROR_MESSAGE = "error registering user";
+
+const RECOVER_ACCOUNT_EMAIL_NOT_FOUND_ON_SERVER = "email not found";
+const RECOVER_ACCOUNT_EMAIL_FIELD_REQUIRED = "email required";
+const RECOVER_ACCOUNT_GENERAL_ERROR_MESSAGE = "error recovering account";
+const RECOVER_ACCOUNT_SUCCESS_MESSAGE = "account recovery successfully, soon you will receive an email with instructions";
