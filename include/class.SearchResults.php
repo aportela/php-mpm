eab571a83f4fa8a1e25df7bc94cf29d33339c93e
alex <766f6964+github@gmail.com>
2016-10-15 11:17:02 +0200

    new class for returning data on search methods

---
+++ include/class.SearchResults.php
@@ -0,0 +1,68 @@
+<?php
+    namespace PHP_MPM;
+
+    /**
+    *   search results class
+    */
+    class SearchResults {
+
+        public $pager = null;
+        public $results = null;
+
+		public function __construct () { 
+            $this->pager = new \stdClass();
+            $this->pager->enabled = false;
+            $this->pager->actualPage = 1;
+            $this->pager->resultspage = 0;
+            $this->pager->totalPages = 1;
+            $this->pager->totalResults = 0;
+            $this->results = array();
+        }
+
+        public function __destruct() { }
+
+        /**
+        *   set pager properties
+        *
+        *   @param int totalResults
+        *       total result count
+        *   @param int actualPage
+        *       actual requested page
+        *   @param int resultsPage
+        *       number of results per page (0 = disable pagination)
+        */
+        public function setPager(int $totalResults = 0, int $actualPage = 1, int $resultsPage = 0) {
+            if ($resultsPage > 0) {
+                $this->pager->enabled = true;
+                $this->pager->totalResults = $totalResults;
+                $this->pager->actualPage = $actualPage;
+                $this->pager->resultspage = $resultsPage;
+                $this->pager->totalPages = ceil($this->pager->totalResults / $this->pager->resultspage);
+                if ($this->pager->actualPage < 0 || $this->pager->actualPage > $this->pager->totalPages) {
+                    throw new \PHP_MPM\MPMInvalidParamsException(print_r(get_object_vars($this), true));
+                }                
+            } else {
+                $this->pager->enabled = false;
+                $this->pager->totalResults = $totalResults;
+                $this->pager->actualPage = $actualPage;
+                $this->pager->resultspage = $resultsPage;
+                $this->pager->totalPages = 1;
+            }
+        }
+
+        /**
+        *   set result data
+        *
+        *   @param array results
+        *       results collection
+        */
+        public function setResults(array $results) {
+            $this->results = $results;
+            if (! $this->pager->enabled) {
+                $this->pager->totalResults = count($results);
+                $this->pager->actualPage = 1;
+                $this->pager->totalPages = 1;
+            }
+        }
+    }
+?>
