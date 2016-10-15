42f7d23d5329fae11bc6ca2957356f6c08c1e330
alex <766f6964+github@gmail.com>
2016-10-15 16:14:38 +0200

    updated form fields for setting max length & required flag

--- templates/default-bulma/app-not_logged.php
+++ templates/default-bulma/app-not_logged.php
@@ -14,41 +14,41 @@
                 </div>            
                 <div class="card-content">
                     <form class="tab-content" id="frm_signin" method="post" action="/api/user/login.php">
-                        <p class="control has-icon" id="c_email">
-                            <input class="input" type="email" name="email" placeholder="Email" value="admin@localhost">
+                        <p class="control has-icon" id="c_signin_email">
+                            <input class="input" type="email" name="email" placeholder="Email" value="admin@localhost" maxlength="254" required autofocus>
                             <i class="fa fa-envelope"></i>
                         </p>
-                        <p class="control has-icon" id="c_password">
-                            <input class="input" type="password" name="password" placeholder="Password" value="password">
+                        <p class="control has-icon" id="c_signin_password">
+                            <input class="input" type="password" name="password" placeholder="Password" value="password" required>
                             <i class="fa fa-lock"></i>
                         </p>
-                        <p class="control">
+                        <p class="control" id="c_signin_submit">
                             <button type="submit" class="button is-success">sign in</button>
                         </p>
                     </form>
                     <form class="tab-content is-hidden" id="frm_signup" method="post" action="/api/user/signup.php">
-                        <p class="control has-icon" id="c_email">
-                            <input class="input" type="email" name="email" placeholder="Email" value="admin@localhost">
+                        <p class="control has-icon" id="c_signup_email">
+                            <input class="input" type="email" name="email" placeholder="Email" value="admin@localhost" maxlength="254" required>
                             <i class="fa fa-envelope"></i>
                         </p>
-                        <p class="control has-icon" id="c_name">
-                            <input class="input" type="text" name="name" placeholder="Name" value="administrator">
+                        <p class="control has-icon" id="c_signup_name">
+                            <input class="input" type="text" name="name" placeholder="Name" value="administrator" maxlength="32" required>
                             <i class="fa fa-user"></i>
                         </p>
-                        <p class="control has-icon" id="c_password">
-                            <input class="input" type="password" name="password" placeholder="Password" value="password">
+                        <p class="control has-icon" id="c_signup_password">
+                            <input class="input" type="password" name="password" placeholder="Password" value="password" required>
                             <i class="fa fa-lock"></i>
                         </p>
-                        <p class="control">
+                        <p class="control" id="c_signup_submit">
                             <button type="submit" class="button is-success">sign up</button>
                         </p>
                     </form>
                     <form class="tab-content is-hidden" id="frm_recover_account" method="post" action="#">
-                        <p class="control has-icon" id="c_email">
-                            <input class="input" type="email" name="email" placeholder="Email" value="admin@localhost">
+                        <p class="control has-icon" id="c_recover_account_email">
+                            <input class="input" type="email" name="email" placeholder="Email" value="admin@localhost" maxlength="254" required>
                             <i class="fa fa-envelope"></i>
                         </p>
-                        <p class="control">
+                        <p class="control" id="c_recover_account_submit">
                             <button type="submit" class="button is-success">recover</button>
                         </p>
                     </form>                    
