c07f1aa01353a5e9517c37afeb285e3e0a570f6b
alex <766f6964+github@gmail.com>
2016-10-15 16:13:27 +0200

    changed invalid date

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 
     ob_start();
 
-    header('Expires: Sun, 01 Jan 2000 00:00:00 GMT');
+    header('Expires: Thu, 19 Nov 1981 00:00:00 GMT');
     header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
     header("Cache-Control: post-check=0, pre-check=0", false);
     header("Pragma: no-cache");    
