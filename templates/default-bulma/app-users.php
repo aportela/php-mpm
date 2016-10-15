5a67b5db0d35a31b56c3610e43bca00fa27a71dd
alex <766f6964+github@gmail.com>
2016-10-15 17:27:21 +0200

    added pagination support in user administration ui

--- templates/default-bulma/app-users.php
+++ templates/default-bulma/app-users.php
@@ -1,19 +1,42 @@
 <table id="users" class="table is-bordered is-narrow">
-  <thead>   
+  <thead>
       <tr>
-        <th colspan="2">
-          <a class="button is-info is-small"><span class="icon"><i class="fa fa-fw fa-arrow-left" aria-hidden="true"></i></span><span>Previous Page</span></a>
-          <a class="button is-info is-small"><span>Next Page</span><span class="icon"><i class="fa fa-fw fa-arrow-right" aria-hidden="true"></i></span></a>
-        </th>
-        <th colspan="4">
+        <form class="frm_search_users" method="post" action="/api/user/search.php">
+        <input type="hidden" name="page" class="i_page" value="1">
+        <th colspan="3">
           <p class="control has-addons">
-            <input class="input is-small" type="text" placeholder="text filter">
-            <a class="button is-info is-small">
-              <span>Search</span><span class="icon"><i class="fa fa-fw fa-search" aria-hidden="true"></i></span>
-            </a>
-          </p>        
+            <span class="select">
+              <select id="s_results_page" name="resultsPage">
+                <option value="2" selected>2 results/page</option>
+                <option value="4">4 results/page</option>
+                <option value="16">16 results/page</option>
+                <option value="32">32 results/page</option>
+                <option value="64">64 results/page</option>
+                <option value="0">no pagination</option>
+              </select>
+            </span>
+            <input class="input is-expanded" type="text" placeholder="text filter">
+          </p>                
         </th>
-      </tr>
+        <th colspan="3">
+          <nav class="pagination">
+            <a class="button is-info btn_previous_page is-disabled">Previous</a>
+            <a class="button is-info btn_next_page is-disabled">Next page</a>
+            <ul>
+              <li>
+                <a class="button is-primary pager_actual_page">1</a>
+              </li>
+              <li>
+                <span>...</span>
+              </li>
+              <li>
+                <a class="button pager_total_pages">1</a>
+              </li>
+            </ul>
+          </nav>        
+        </th>
+        </form>
+      </tr>   
     <tr>
         <th>Operations</th>
         <th>Type</th>
--- templates/default-bulma/static/scripts/app-users.js
+++ templates/default-bulma/static/scripts/app-users.js
@@ -1,7 +1,8 @@
-function refreshUsersTable() {
+$("form.frm_search_users").submit(function (e) {
+    e.preventDefault();
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "/api/user/search.php", true);
-    xhr.onreadystatechange = function(e) {
+    xhr.onreadystatechange = function (e) {
         if (this.readyState == 4) {
             var response = null;
             try {
@@ -18,6 +19,18 @@ function refreshUsersTable() {
                             // TODO: error
                             console.log(this.status);
                         } else {
+                            $(".pager_actual_page").text(response.data.pager.actualPage);
+                            $(".pager_total_pages").text(response.data.pager.totalPages);
+                            if (response.data.pager.actualPage < response.data.pager.totalPages) {
+                                $(".btn_next_page").removeClass("is-disabled");
+                            } else {
+                                $(".btn_next_page").addClass("is-disabled");
+                            }
+                            if (response.data.pager.actualPage > 1) {
+                                $(".btn_previous_page").removeClass("is-disabled");
+                            } else {
+                                $(".btn_previous_page").addClass("is-disabled");
+                            }
                             var html = null;
                             if (response.data && response.data.results.length > 0) {
                                 for (var i = 0; i < response.data.results.length; i++) {
@@ -46,17 +59,14 @@ function refreshUsersTable() {
             }
         }
     }
-    var formData = new FormData();
-    formData.append("page", 1);
-    formData.append("resultsPage", 16);
-    xhr.send(formData, null, 2);
-}
+    xhr.send(new FormData($(this)[0]), null, 2);
+});
 
-$("form#frm_delete_user").submit(function(e) {
+$("form#frm_delete_user").submit(function (e) {
     e.preventDefault();
     var xhr = new XMLHttpRequest();
     xhr.open($(this).attr("method"), $(this).attr("action"), true);
-    xhr.onreadystatechange = function(e) {
+    xhr.onreadystatechange = function (e) {
         if (this.readyState == 4) {
             var response = null;
             try {
@@ -71,13 +81,11 @@ $("form#frm_delete_user").submit(function(e) {
                     case 200:
                         $('html').removeClass('is-clipped');
                         $('div.modal').removeClass('is-active');
-                        refreshUsersTable();
+                        $("form.frm_search_users").submit();
                         break;
                     default:
                         $(".modal_error").removeClass("is-hidden");
                         $(".modal_error .message-body").text("operation error");
-                        // TODO: error
-                        console.log(this.status);
                         break;
                 }
             }
@@ -86,12 +94,12 @@ $("form#frm_delete_user").submit(function(e) {
     xhr.send(new FormData($(this)[0]), null, 2);
 });
 
-$("form#frm_update_user").submit(function(e) {
+$("form#frm_update_user").submit(function (e) {
     e.preventDefault();
     e.stopPropagation();
     var xhr = new XMLHttpRequest();
     xhr.open($(this).attr("method"), $(this).attr("action"), true);
-    xhr.onreadystatechange = function(e) {
+    xhr.onreadystatechange = function (e) {
         if (this.readyState == 4) {
             var response = null;
             try {
@@ -106,13 +114,11 @@ $("form#frm_update_user").submit(function(e) {
                     case 200:
                         $('html').removeClass('is-clipped');
                         $('div.modal').removeClass('is-active');
-                        refreshUsersTable();
+                        $("form.frm_search_users").submit();
                         break;
                     default:
                         $(".modal_error").removeClass("is-hidden");
                         $(".modal_error .message-body").text("operation error");
-                        // TODO: error
-                        console.log(this.status);
                         break;
                 }
             }
@@ -121,13 +127,47 @@ $("form#frm_update_user").submit(function(e) {
     xhr.send(new FormData($(this)[0]), null, 2);
 });
 
-$('table tbody').on("click", ".btn_delete_user", function(e) {
+$('table tbody').on("click", ".btn_delete_user", function (e) {
     $("input#delete_user_id").val($(this).closest("tr").data("id"));
     $("strong#delete_user_name").text($(this).closest("tr").find("td:nth-child(3)").text());
 });
 
-$('table tbody').on("click", ".btn_update_user", function(e) {
+$('table tbody').on("click", ".btn_update_user", function (e) {
     $("input#update_user_id").val($(this).closest("tr").data("id"));
     $("input#update_user_email").val($(this).closest("tr").find("td:nth-child(4)").text());
     $("input#update_user_name").val($(this).closest("tr").find("td:nth-child(3)").text());
 });
+
+$(".btn_previous_page").click(function (e) {
+    var v = parseInt($(".i_page").val());
+    v--;
+    if (v < 1) {
+        v = 1;
+        $(this).addClass("is-disabled");
+    } else {
+        $(this).removeClass("is-disabled");
+    }
+    $(".i_page").val(v);
+    $("form.frm_search_users").submit();
+});
+
+$(".btn_next_page").click(function (e) {
+    var v = parseInt($(".i_page").val());
+    var totalPages = parseInt($(".pager_total_pages").text());
+    v++;
+    if (v > totalPages) {
+        b = totalPages;
+        $(this).addClass("is-disabled");
+    } else {
+        $(this).removeClass("is-disabled");
+    }
+    $(".i_page").val(v);
+    $("form.frm_search_users").submit();
+});
+
+$("select#s_results_page").change(function (e) {
+    $(".i_page").val(1);
+    $("form.frm_search_users").submit();
+});
+
+$("form.frm_search_users").submit();
