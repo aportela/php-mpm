7da05eb37887385300b4a2fa8ec57bf4b8dc164d
alex <766f6964+github@gmail.com>
2016-10-16 01:20:12 +0200

    updated table header form controls for fixed size allocation

--- templates/default-bulma/app-groups.php
+++ templates/default-bulma/app-groups.php
@@ -3,48 +3,52 @@
     <tr>
       <form class="frm_search_groups" method="post" action="/api/group/search.php">
       <input type="hidden" name="page" class="i_page" value="1">
-      <th colspan="1">
-        <p class="control has-addons has-addons-right">
-          <span class="select">
-            <select id="export_table_data_format">
-              <option value="">select format</option>
-              <option value="json">json</option>
-              <option value="xml">xml</option>
-            </select>
-          </span>
-          <a id="btn_export_table_data" class="button is-primary is-disabled">Export data</a>
-        </p>
-      </th>
-      <th colspan="3">
-        <p class="control has-addons">
-          <span class="select">
-            <select id="s_results_page" name="resultsPage">
-              <option value="8">8 results/page</option>
-              <option value="16" selected>16 results/page</option>
-              <option value="32">32 results/page</option>
-              <option value="64">64 results/page</option>
-              <option value="0">no pagination</option>
-            </select>
-          </span>
-          <input class="input is-expanded is-disabled" type="text" placeholder="text filter (TODO)">
-        </p>                
-      </th>
-      <th colspan="2">
-        <nav class="pagination">
-          <a class="button is-info btn_previous_page is-disabled">Previous</a>
-          <a class="button is-info btn_next_page is-disabled">Next page</a>
-          <ul>
-            <li>
-              <a class="button is-primary pager_actual_page is-disabled">1</a>
-            </li>
-            <li>
-              <span>...</span>
-            </li>
-            <li>
-              <a class="button pager_total_pages is-disabled">1</a>
-            </li>
-          </ul>
-        </nav>        
+      <th colspan="6">
+        <div class="columns">
+          <div class="column is-2">
+            <p class="control has-addons has-addons-right">
+              <span class="select">
+                <select id="export_table_data_format">
+                  <option value="">select format</option>
+                  <option value="json">json</option>
+                  <option value="xml">xml</option>
+                </select>
+              </span>
+              <a id="btn_export_table_data" class="button is-primary is-disabled">Export data</a>
+            </p>          
+          </div>
+          <div class="column is-5">
+            <p class="control has-addons">
+              <span class="select">
+                <select id="s_results_page" name="resultsPage">
+                  <option value="8">8 results/page</option>
+                  <option value="16" selected>16 results/page</option>
+                  <option value="32">32 results/page</option>
+                  <option value="64">64 results/page</option>
+                  <option value="0">no pagination</option>
+                </select>
+              </span>
+              <input class="input is-expanded is-disabled" type="text" placeholder="text filter (TODO)">
+            </p>                          
+          </div>
+          <div class="column is-5">
+            <nav class="pagination">
+              <a class="button is-info btn_previous_page is-disabled">Previous</a>
+              <a class="button is-info btn_next_page is-disabled">Next page</a>
+              <ul>
+                <li>
+                  <a class="button is-primary pager_actual_page is-disabled">1</a>
+                </li>
+                <li>
+                  <span>...</span>
+                </li>
+                <li>
+                  <a class="button pager_total_pages is-disabled">1</a>
+                </li>
+              </ul>
+            </nav>                  
+          </div>
+        </div>      
       </th>
       </form>
     </tr>   
