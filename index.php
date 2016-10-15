61b364d77162ebf6f8bf9dcda973d83b8e724bb9
alex <766f6964+github@gmail.com>
2016-10-15 15:07:24 +0200

    dont cache (for testing)

--- index.php
+++ index.php
@@ -4,6 +4,12 @@
     require_once __DIR__ . DIRECTORY_SEPARATOR . "include" . "/configuration.php";
 
     ob_start();
+
+    header('Expires: Sun, 01 Jan 2000 00:00:00 GMT');
+    header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+    header("Cache-Control: post-check=0, pre-check=0", false);
+    header("Pragma: no-cache");    
+
     session_start();
 
     $t = new \PHP_MPM\Template("layout.php");
