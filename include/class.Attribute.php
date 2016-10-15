339abe0cc006862a9255a51319c9adeb366821ca
alex <766f6964+github@gmail.com>
2016-10-15 20:44:43 +0200

    pagination support in attribute class search method

--- include/class.Attribute.php
+++ include/class.Attribute.php
@@ -69,8 +69,21 @@
                 throw new \PHP_MPM\MPMAuthSessionRequiredException("");
             } else {
                 $data = new \PHP_MPM\SearchResults();
-                $data->setPager(0, 1, 0);                
-                $data->setResults(\PHP_MPM\Database::execWithResult(" SELECT A.id, A.name, A.description, A.type, U.id AS creatorId, U.name AS creatorName, datetime(A.created, 'localtime') AS creationDate FROM [ATTRIBUTE] A LEFT JOIN [USER] U ON U.id = A.creator ORDER BY A.name ", array()));
+                if ($resultsPage > 0) {
+                    $totalResults = \PHP_MPM\Database::execScalar(" SELECT COUNT(A.id) FROM [ATTRIBUTE] A ", array());
+                    $data->setPager($totalResults, $page, $resultsPage);
+                    $params = array();
+                    $param = new \PHP_MPM\DatabaseParam();
+                    $param->int(":start", (($page - 1) * $resultsPage));
+                    $params[] = $param;
+                    $param = new \PHP_MPM\DatabaseParam();
+                    $param->int(":results_page", $resultsPage);
+                    $params[] = $param;                    
+                    $data->setResults(\PHP_MPM\Database::execWithResult(" SELECT A.id, A.name, A.description, A.type, U.id AS creatorId, U.name AS creatorName, datetime(A.created, 'localtime') AS creationDate FROM [ATTRIBUTE] A LEFT JOIN [USER] U ON U.id = A.creator ORDER BY A.name LIMIT :start, :results_page ", $params));
+                } else {
+                    $data->setPager(0, 1, 0);
+                    $data->setResults(\PHP_MPM\Database::execWithResult(" SELECT A.id, A.name, A.description, A.type, U.id AS creatorId, U.name AS creatorName, datetime(A.created, 'localtime') AS creationDate FROM [ATTRIBUTE] A LEFT JOIN [USER] U ON U.id = A.creator ORDER BY A.name ", array()));
+                }                                
                 return($data);
             }
         }
