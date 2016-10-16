eec4725b824e8db333f781a0d48d24d81fa637fb
alex <766f6964+github@gmail.com>
2016-10-16 16:59:10 +0200

    check api response success json property

--- templates/default-bulma/static/scripts/app-users.js
+++ templates/default-bulma/static/scripts/app-users.js
@@ -1,26 +1,26 @@
 $("form.frm_search_users").submit(function(e) {
     e.preventDefault();
-    mpm.form.submit(this, function(httpStatusCode, result) {
+    mpm.form.submit(this, function(httpStatusCode, response) {
         switch (httpStatusCode) {
             case 200:
-                if (result === null) {
+                if (!(response && response.success)) {
                     mpm.error.showModal();
                 } else {
-                    mpm.pagination.setControls(result.data.pager.actualPage, result.data.pager.totalPages);
+                    mpm.pagination.setControls(response.data.pager.actualPage, response.data.pager.totalPages);
                     var html = null;
-                    if (result.data && result.data.results.length > 0) {
-                        for (var i = 0; i < result.data.results.length; i++) {
-                            html += '<tr data-id="' + result.data.results[i].id + '">';
+                    if (response.data && response.data.results.length > 0) {
+                        for (var i = 0; i < response.data.results.length; i++) {
+                            html += '<tr data-id="' + response.data.results[i].id + '">';
                             html += '<td class="has-text-centered ignore_on_export"><a class="button is-small is-info modal-button btn_update_user" data-target="#modal_update">Update</a> <a class="button is-small is-danger modal-button btn_delete_user"  data-target="#modal_delete">Delete</a></td>'
-                            if (result.data.results[i].type == 1) {
+                            if (response.data.results[i].type == 1) {
                                 html += '<td ><span class="icon is-small"><i class="fa fa-1x fa-user-md" aria-hidden="true"></i></span> <span>super</span></td>';
                             } else {
                                 html += '<td><span class="icon is-small"><i class="fa fa-1x fa-user" aria-hidden="true"></i></span> <span>normal</span></td>';
                             }
-                            html += '<td  class="is-small">' + result.data.results[i].name + '</td>';
-                            html += '<td><a href="mailto:' + result.data.results[i].email + '">' + result.data.results[i].email + '<a/></td>';
-                            html += '<td data-id="' + result.data.results[i].creatorId + '">' + (result.data.results[i].creatorId != result.data.results[i].id ? result.data.results[i].creatorName : "auto-register") + '</td>';
-                            html += '<td data-date="' + result.data.results[i].creationDate + '">' + new moment(result.data.results[i].creationDate).fromNow() + '</td>';
+                            html += '<td  class="is-small">' + response.data.results[i].name + '</td>';
+                            html += '<td><a href="mailto:' + response.data.results[i].email + '">' + response.data.results[i].email + '<a/></td>';
+                            html += '<td data-id="' + response.data.results[i].creatorId + '">' + (response.data.results[i].creatorId != response.data.results[i].id ? response.data.results[i].creatorName : "auto-register") + '</td>';
+                            html += '<td data-date="' + response.data.results[i].creationDate + '">' + new moment(response.data.results[i].creationDate).fromNow() + '</td>';
                             html += '</tr>';
                         }
                     }
@@ -36,15 +36,19 @@ $("form.frm_search_users").submit(function(e) {
 
 $("form#frm_add_user").submit(function(e) {
     e.preventDefault();
-    mpm.form.submit(this, function(httpStatusCode, result) {
+    mpm.form.submit(this, function(httpStatusCode, response) {
         switch (httpStatusCode) {
             case 409:
                 mpm.form.putValidationError("ca_email", USER_ADD_EMAIL_EXISTS);
                 break;
             case 200:
-                $('html').removeClass('is-clipped');
-                $('div.modal').removeClass('is-active');
-                $("form.frm_search_users").submit();
+                if (!(response && response.success)) {
+                    mpm.error.showModal();
+                } else {
+                    $('html').removeClass('is-clipped');
+                    $('div.modal').removeClass('is-active');
+                    $("form.frm_search_users").submit();
+                }
                 break;
             default:
                 mpm.error.showModal();
@@ -56,12 +60,16 @@ $("form#frm_add_user").submit(function(e) {
 
 $("form#frm_delete_user").submit(function(e) {
     e.preventDefault();
-    mpm.form.submit(this, function(httpStatusCode, result) {
+    mpm.form.submit(this, function(httpStatusCode, response) {
         switch (httpStatusCode) {
             case 200:
-                $('html').removeClass('is-clipped');
-                $('div.modal').removeClass('is-active');
-                $("form.frm_search_users").submit();
+                if (!(response && response.success)) {
+                    mpm.error.showModal();
+                } else {
+                    $('html').removeClass('is-clipped');
+                    $('div.modal').removeClass('is-active');
+                    $("form.frm_search_users").submit();
+                }
                 break;
             default:
                 mpm.error.showModal();
@@ -72,15 +80,19 @@ $("form#frm_delete_user").submit(function(e) {
 
 $("form#frm_update_user").submit(function(e) {
     e.preventDefault();
-    mpm.form.submit(this, function(httpStatusCode, result) {
+    mpm.form.submit(this, function(httpStatusCode, response) {
         switch (httpStatusCode) {
             case 409:
                 mpm.form.putValidationError("cu_email", USER_UPDATE_EMAIL_EXISTS);
                 break;
             case 200:
-                $('html').removeClass('is-clipped');
-                $('div.modal').removeClass('is-active');
-                $("form.frm_search_users").submit();
+                if (!(response && response.success)) {
+                    mpm.error.showModal();
+                } else {
+                    $('html').removeClass('is-clipped');
+                    $('div.modal').removeClass('is-active');
+                    $("form.frm_search_users").submit();
+                }
                 break;
             default:
                 mpm.error.showModal();
