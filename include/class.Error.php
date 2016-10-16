f489337a0112dfe3f035d45ceeb798baa465ee5d
alex <766f6964+github@gmail.com>
2016-10-16 11:11:04 +0200

    new fields added on error class

--- include/class.Error.php
+++ include/class.Error.php
@@ -25,10 +25,26 @@
             $param->int(":code", intval($e->getCode()));
             $params[] = $param;
             $param = new \PHP_MPM\DatabaseParam();
+            $param->str(":message", $e->getMessage());
+            $params[] = $param;            
+            $param = new \PHP_MPM\DatabaseParam();
             $param->str(":trace", print_r($e->getTrace(), true));
             $params[] = $param;
+            $param = new \PHP_MPM\DatabaseParam();
+            if (\PHP_MPM\User::isAuthenticated()) {
+                $param->str(":user_id", \PHP_MPM\User::getSessionUserId());                
+            } else {
+                $param->null(":user_id");
+            }
+            $params[] = $param;
+            $param = new \PHP_MPM\DatabaseParam();
+            $param->str(":user_agent", \PHP_MPM\Utils::getBrowserUserAgent());
+            $params[] = $param;            
+            $param = new \PHP_MPM\DatabaseParam();
+            $param->str(":user_remote_address", \PHP_MPM\Utils::getRemoteIpAddress());
+            $params[] = $param;            
             try {
-                \PHP_MPM\Database::execWithoutResult(" INSERT INTO ERROR (created, class, line, filename, code, trace) VALUES (CURRENT_TIMESTAMP, :class, :line, :filename, :code, :trace) ", $params);
+                \PHP_MPM\Database::execWithoutResult(" INSERT INTO ERROR (created, class, line, filename, code, message, trace, user_id, user_agent, user_remote_address) VALUES (CURRENT_TIMESTAMP, :class, :line, :filename, :code, :message, :trace, :user_id, :user_agent, :user_remote_address) ", $params);
             } catch (\Throwable $e) {
                 // we do not want to throw (again) on error 
             }
@@ -38,9 +54,9 @@
         *   search (list) errors
         */
         public static function search($page, $resultsPage) {
-            if (! User::isAuthenticated()) {
+            if (! \PHP_MPM\User::isAuthenticated()) {
                 throw new \PHP_MPM\MPMAuthSessionRequiredException(print_r(get_object_vars($this), true));
-            } else if (! User::isAuthenticatedAsAdmin()) {
+            } else if (! \PHP_MPM\User::isAuthenticatedAsAdmin()) {
                 throw new \PHP_MPM\MPMAdminPrivilegesRequiredException(print_r(get_object_vars($this), true));
             } else {
                 $data = new \PHP_MPM\SearchResults();
@@ -54,10 +70,10 @@
                     $param = new \PHP_MPM\DatabaseParam();
                     $param->int(":results_page", $resultsPage);
                     $params[] = $param;                    
-                    $data->setResults(\PHP_MPM\Database::execWithResult(" SELECT datetime(created, 'localtime') AS creationDate, class, line, filename, code, trace FROM [ERROR] ORDER BY created DESC LIMIT :start, :results_page ", $params));
+                    $data->setResults(\PHP_MPM\Database::execWithResult(" SELECT datetime(E.created, 'localtime') AS creationDate, E.class, E.line, E.filename, E.code, E.message, E.trace, E.user_agent as userAgent, E.user_remote_address as remoteAddress, U.id AS userId, U.name AS userName FROM [ERROR] E LEFT JOIN [USER] U ON U.id = user_id ORDER BY E.created DESC LIMIT :start, :results_page ", $params));
                 } else {
                     $data->setPager(0, 1, 0);
-                    $data->setResults(\PHP_MPM\Database::execWithResult(" SELECT datetime(created, 'localtime') AS creationDate, class, line, filename, code, trace FROM [ERROR] ORDER BY created DESC ", array()));
+                    $data->setResults(\PHP_MPM\Database::execWithResult(" SELECT datetime(E.created, 'localtime') AS creationDate, E.class, E.line, E.filename, E.code, E.message, E.trace, E.user_agent as userAgent, E.user_remote_address as remoteAddress, U.id AS userId, U.name AS userName FROM [ERROR] E LEFT JOIN [USER] U ON U.id = user_id ORDER BY E.created DESC ", array()));
                 }                                
                 return($data);                
             }
