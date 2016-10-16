bf0126b6320ddf48b52529598beafb5bd84d59ae
alex <766f6964+github@gmail.com>
2016-10-16 19:43:45 +0200

    enabled fast filter by text on group ui

--- templates/default-bulma/static/scripts/app-groups.js
+++ templates/default-bulma/static/scripts/app-groups.js
@@ -143,4 +143,15 @@ $("select#s_results_page").change(function(e) {
     $("form.frm_search_groups").submit();
 });
 
+var timer = null;
+$("input#fast_search_filter").keyup(function(e) {
+    e.preventDefault();
+    if (timer) {
+        clearTimeout(timer);
+    }
+    timer = setTimeout(function() {
+        $("form.frm_search_groups").submit();
+    }, 500);
+});
+
 $("form.frm_search_groups").submit();
