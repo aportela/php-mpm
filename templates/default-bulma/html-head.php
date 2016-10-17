b89122aa515fff7da66eb7a58e841a8362fafa8b
alex <766f6964+github@gmail.com>
2016-10-17 19:48:47 +0200

    moved (& documented) styles into to new file

--- templates/default-bulma/html-head.php
+++ templates/default-bulma/html-head.php
@@ -8,5 +8,5 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.2.1/css/bulma.min.css">
     <!-- font awesome icons -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="/templates/default-bulma/static/styles/style.css">
+    <link rel="stylesheet" href="/templates/default-bulma/static/styles/app-common.css">
 </head>
