e7a38b969f5eb1f99f9f01fc93fe5cad6f6da78c
alex <766f6964+github@gmail.com>
2016-10-22 19:42:49 +0200

    new method for generating uuids

--- templates/default-bulma/static/scripts/mpm.js
+++ templates/default-bulma/static/scripts/mpm.js
@@ -2,6 +2,21 @@
 
 var mpm = mpm || {};
 
+mpm.utils = mpm.utils || {};
+
+/**
+ * uuid generator
+ * (broofa) http://stackoverflow.com/a/2117523
+ */
+mpm.util.uuid = function() {
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16 | 0,
+            v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+    return (uuid);
+}
+
 mpm.form = mpm.form || {};
 
 mpm.form.disableSubmit = function(form) {
