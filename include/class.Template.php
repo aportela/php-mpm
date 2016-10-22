ee40ef76a75f526b5592698449f7353f8b3f753e
alex <766f6964+github@gmail.com>
2016-10-22 19:26:56 +0200

    remove local references to template permissions methods

--- include/class.Template.php
+++ include/class.Template.php
@@ -97,10 +97,8 @@
                 $params[] = $param;
                 // TODO: transaction support
                 \PHP_MPM\Database::execWithoutResult(" INSERT INTO [TEMPLATE] (id, name, description, created, creator) VALUES (:id, :name, :description, CURRENT_TIMESTAMP, :creator) ", $params);
-                if ($this->permissions && count($this->permissions) > 0) {
-                    foreach($this->permissions as $permission) {
-                        $this->addPermission($permission->group->id, $permission->allowCreate, $permission->allowView, $permission->allowUpdate, $permission->allowDelete);
-                    }
+                foreach($this->permissions as $permission) {
+                    $permission->add($this->id);
                 }
                 if ($this->attributes && count($this->attributes) > 0) {
                     foreach($this->attributes as $templateAttribute) {
@@ -145,11 +143,11 @@
                 // TODO: transaction support
                 \PHP_MPM\Database::execWithoutResult(" UPDATE [TEMPLATE] SET name = :name, description = :description WHERE id = :id ", $params);
                 // TODO: better check user diffs ¿?
-                $this->removeAllPermissions();
+                \PHP_MPM\TemplatePermission::deleteAll($this->id);
                 foreach($this->permissions as $permission) {
-                    $this->addPermission($permission->group->id, $permission->allowCreate, $permission->allowView, $permission->allowUpdate, $permission->allowDelete);
+                    $permission->add($this->id);
                 }
-                \PHP_MPM\TemplateAttribute::deleteAllTemplateAttributes($this->id);
+                \PHP_MPM\TemplateAttribute::deleteAll($this->id);
                 foreach($this->attributes as $templateAttribute) {
                     if (empty($templateAttribute->id)) {
                         $templateAttribute->id = \PHP_MPM\Utils::uuid();
@@ -159,67 +157,7 @@
             }
         }
 
-        /**
-        *   add group to template
-        */
-        private function addPermission($groupId, $allowCreate = true, $allowView = true, $allowUpdate = true, $allowDelete = true) {
-            if (empty($groupId)) {
-                throw new \PHP_MPM\MPMInvalidParamsException(print_r(get_object_vars($this), true));
-            } else {
-                $g = new \PHP_MPM\Group();
-                $g->set($groupId, "", "", 0);
-                if (! $g->exists()) {
-                    throw new \PHP_MPM\MPMNotFoundException(print_r(get_object_vars($this), true));
-                } else {
-                    $params = array();
-                    $param = new \PHP_MPM\DatabaseParam();
-                    $param->str(":template_id", $this->id);
-                    $params[] = $param;                            
-                    $param = new \PHP_MPM\DatabaseParam();
-                    $param->str(":group_id", $groupId);
-                    $params[] = $param;                                
-                    $param = new \PHP_MPM\DatabaseParam();
-                    $param->bool(":allow_create", $allowCreate);
-                    $params[] = $param;
-                    $param = new \PHP_MPM\DatabaseParam();
-                    $param->bool(":allow_view", $allowView);
-                    $params[] = $param;
-                    $param = new \PHP_MPM\DatabaseParam();
-                    $param->bool(":allow_update", $allowUpdate);
-                    $params[] = $param;
-                    $param = new \PHP_MPM\DatabaseParam();
-                    $param->bool(":allow_delete", $allowDelete);
-                    $params[] = $param;
-                    \PHP_MPM\Database::execWithoutResult(" INSERT INTO [TEMPLATE_PERMISSION] (template_id, group_id, allow_create, allow_view, allow_update, allow_delete) VALUES (:template_id, :group_id, :allow_create, :allow_view, :allow_update, :allow_delete) ", $params);
-                }
-            }            
-        }
-
-        /**
-        * remove group from template
-        */
-        private function removePermission($groupId) {
-            $params = array();
-            $param = new \PHP_MPM\DatabaseParam();
-            $param->str(":template_id", $this->id);
-            $params[] = $param;
-            $param = new \PHP_MPM\DatabaseParam();
-            $param->str(":group_id", $groupId);
-            $params[] = $param;
-            \PHP_MPM\Database::execWithoutResult(" DELETE FROM [TEMPLATE_PERMISSION] WHERE template_id = :template_id AND group_id = :group_id ", $params);
-        }
-
-        /**
-        *   remove all groups from template
-        */
-        private function removeAllPermissions() {
-            $params = array();
-            $param = new \PHP_MPM\DatabaseParam();
-            $param->str(":template_id", $this->id);
-            $params[] = $param;                                
-            \PHP_MPM\Database::execWithoutResult(" DELETE FROM [TEMPLATE_PERMISSION] WHERE template_id = :template_id ", $params);
-        }        
-        
+             
         /**
         *   search (list) templates
         */
@@ -281,37 +219,14 @@
                 throw new \PHP_MPM\MPMAdminPrivilegesRequiredException(print_r(get_object_vars($this), true));
             } else {
                 // TODO: transaction support
-                $this->removeAllPermissions();
                 $params = array();
                 $param = new \PHP_MPM\DatabaseParam();
                 $param->str(":id", $this->id);
                 $params[] = $param;                                
                 \PHP_MPM\Database::execWithoutResult(" DELETE FROM [TEMPLATE] WHERE id = :id ", $params);
-                // TODO: delete template permissions & attributes (references)
+                \PHP_MPM\TemplateAttribute::deleteAll($this->id);
+                \PHP_MPM\TemplatePermission::deleteAll($this->id);
             }
-        }
-
-        /**
-        *   get template permissions
-        */
-        private function getPermissions() {
-            $param = new \PHP_MPM\DatabaseParam();
-            $param->str(":template_id", $this->id);
-            $results = \PHP_MPM\Database::execWithResult(" SELECT TP.group_id AS id, G.name, G.description, TP.allow_create AS allowCreate, TP.allow_update AS allowUpdate, TP.allow_view AS allowView, TP.allow_delete AS allowDelete FROM [TEMPLATE_PERMISSION] TP LEFT JOIN [GROUP] G ON G.id = TP.group_id WHERE TP.template_id = :template_id ", array($param));
-            $this->permissions = array();
-            foreach($results as $result) {
-
-                $permission = new \stdClass;
-                $permission->group = new \stdClass;
-                $permission->group->id = $result->id;
-                $permission->group->name = $result->name;
-                $permission->group->description = $result->description;
-                $permission->allowCreate = $result->allowCreate == 1;
-                $permission->allowView = $result->allowView == 1;
-                $permission->allowUpdate = $result->allowUpdate == 1;
-                $permission->allowDelete = $result->allowDelete == 1;
-                $this->permissions[] = $permission; 
-            }                 
         }
 
         /**
@@ -333,8 +248,8 @@
                 } else {
                     $this->name = $rows[0]->name;
                     $this->description = $rows[0]->description;
-                    $this->getPermissions();
                     $this->attributes = \PHP_MPM\TemplateAttribute::getTemplateAttributes($this->id);
+                    $this->permissions = \PHP_MPM\TemplatePermission::getPermissions($this->id);
                     return(get_object_vars($this));
                 }
             }
