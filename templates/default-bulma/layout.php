c07c1326c4476eac50f278eb859e3707972c4d2b
alex <766f6964+github@gmail.com>
2016-10-22 16:52:26 +0200

    added simple modal with debug info

--- templates/default-bulma/layout.php
+++ templates/default-bulma/layout.php
@@ -4,7 +4,17 @@
   <body>
     <i id="ajax_icon" class="fa fa-cog fa-spin fa-2x fa-fw"></i>
     <nav class="nav" id="top_bar">
-        <div class="nav-left">        
+        <div class="nav-left">
+                <?php
+                  if (ENVIRONMENT_DEV && DEBUG) {
+                ?>
+          <a class="nav-item modal-button" href="#" data-target="#modal_debug" title="click to open modal debug info">
+              <span class="icon"><i class="fa fa-bug"></i></span>
+          </a>
+                
+                <?php
+                  }
+                ?>
         </div>
         <div class="nav-center">
             <span class="nav-item">php-mpm</span>
@@ -14,12 +24,12 @@
         </div>
         <div class="nav-right">
             <?php
-            if ($_TEMPLATE["session_user_is_logged"] == true) {
+              if ($_TEMPLATE["session_user_is_logged"] == true) {
             ?>
             <a class="nav-item" href="#" data-id="<?= $_TEMPLATE["session_user_id"] ?>" data-admin="<?= $_TEMPLATE["session_user_is_admin"] ?>"><span class="icon"><i class="fa fa-user" aria-hidden="true"></i></span> Signed as <?= $_TEMPLATE["session_user_name"] ?></a>
             <a class="nav-item" id="signout" href="/api/user/signout.php"><span class="icon"><i class="fa fa-sign-out" aria-hidden="true"></i></span> Sign Out</a>
             <?php
-            }
+              }
             ?>
         </div>
     </nav>  
@@ -54,7 +64,59 @@
           <a class="button modal_close">Close</a>
         </footer>
       </div>
-    </div>    
+    </div>
+
+    <?php
+      if (ENVIRONMENT_DEV && DEBUG) {
+    ?>
+    <div class="modal" id="modal_debug">
+      <div class="modal-background"></div>
+      <div class="modal-card modal-card_xl">
+        <header class="modal-card-head">
+          <p class="modal-card-title">Debug</p>
+          <button class="delete modal_close"></button>
+        </header>
+        <section class="modal-card-body">      
+          <div class="columns">
+            <div class="column">
+              <div class="card">
+                <header class="card-header">$_SESSION</header>
+                <div class="card-content">
+                  <pre>
+                    <?= print_r($_SESSION, true); ?>
+                  </pre>
+                  </div>
+              </div>
+            </div>
+            <div class="column">
+              <div class="card">
+                <header class="card-header"><?= ($_SERVER["REQUEST_METHOD"] == "GET" ? '$_GET': '$_POST') ?></header>
+                <div class="card-content">
+                  <pre>
+                    <?php
+                      switch($_SERVER["REQUEST_METHOD"]) {
+                        case "GET":
+                          echo print_r($_GET, true);
+                        break;
+                        case "POST":
+                          echo print_r($_POST, true);
+                        break;
+                      }
+                    ?>
+                  </pre>
+                  </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <footer class="modal-card-foot">
+          <a class="button modal_close">Close</a>
+        </footer>        
+      </div>
+    </div>
+    <?php
+      }
+    ?>    
     <?php include "html-scripts.php"; ?>
   </body>
 </html>
