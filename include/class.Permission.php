8311286889a908f3ba10ba6b4f376f0924a8a8af
alex <766f6964+github@gmail.com>
2016-10-22 19:25:41 +0200

    changed permission flags collection into separated property

--- include/class.Permission.php
+++ include/class.Permission.php
@@ -9,27 +9,25 @@
     class Permission {
 
         public $group;
-        public $allowCreate;
-        public $allowView;
-        public $allowUpdate;
-        public $allowDelete;
+        public $flags;
 
 		public function __construct () {
-            $this->group = new \PHP_MPM\Group(); 
-            $this->allowCreate = false;
-            $this->allowView = false;
-            $this->allowUpdate = false;
-            $this->allowDelete = false;
+            $this->group = new \PHP_MPM\Group();
+            $this->flags = new \stdClass();
+            $this->flags->allowCreate = false;
+            $this->flags->allowView = false;
+            $this->flags->allowUpdate = false;
+            $this->flags->allowDelete = false;
         }
 
         public function __destruct() { }
 
         public function set($group, $allowCreate = false, $allowView = false, $allowUpdate = false, $allowDelete = false) {
             $this->group = $group;
-            $this->allowCreate = $allowCreate;
-            $this->allowView = $allowView;
-            $this->allowUpdate = $allowUpdate;
-            $this->allowDelete = $allowDelete;
+            $this->flags->allowCreate = boolval($allowCreate);
+            $this->flags->allowView = boolval($allowView);
+            $this->flags->allowUpdate = boolval($allowUpdate);
+            $this->flags->allowDelete = boolval($allowDelete);
         }
     }
 
