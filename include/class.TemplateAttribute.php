5c3ecaa2faac560e98a59b10a6c2b94f02492ae4
alex <766f6964+github@gmail.com>
2016-10-22 16:56:51 +0200

    added new property (label)

--- include/class.TemplateAttribute.php
+++ include/class.TemplateAttribute.php
@@ -10,6 +10,7 @@
 
         public $id;
         public $attribute;
+        public $label;
         public $required;
         public $defaultValue;
 
@@ -19,9 +20,10 @@
 
         public function __destruct() { }
 
-        public function set($id, $attribute, $required = false, $defaultValue = null) {
+        public function set($id, $attribute, $label = "", $required = false, $defaultValue = null) {
             $this->id = $id;
             $this->attribute = $attribute;
+            $this->label = $label;
             $this->required = $required;
             $this->defaultValue = $defaultValue;
         }
