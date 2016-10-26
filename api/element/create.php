b603cf11f2165c05101bf8ac6064a37e9ca1666f
alex <766f6964+github@gmail.com>
2016-10-26 20:42:38 +0200

    api method for creating (from template) elements

---
+++ api/element/create.php
@@ -0,0 +1,72 @@
+<?php
+    /**
+    *   /api/element/create.php
+    *   description: create element from template
+    *
+    *   request method: POST
+    *   format: json
+    *
+    *   @param string templateId 
+    */
+    namespace PHP_MPM;
+
+    require_once "../../include/configuration.php";
+    require_once "../../include/class.Element.php";
+    require_once "../../include/class.CustomExceptions.php";
+    require_once "../../include/class.Error.php";
+
+    ob_start();
+
+    session_start();
+
+    $result = array("success" => false, "data" => null);
+    try {
+        $params = \PHP_MPM\Utils::getRequestParamsFromJSON();
+        $e = new \PHP_MPM\Element();
+        $e->templateId = isset($params["templateId"]) ? $params["templateId"]: "";
+        $e->create(); 
+        $result["data"] = $e; 
+        $result["success"] = true;
+        ob_clean();
+        header("HTTP/1.1 200 OK", 200, true);
+    } catch (\PHP_MPM\MPMInvalidParamsException $e) {
+        \PHP_MPM\Error::save($e);
+        ob_clean();
+        header("HTTP/1.1 400 Bad Request", 400, true);
+        if (ENVIRONMENT_DEV && DEBUG) {
+            $result["exception"] = print_r($e, true);
+        }
+    } catch (\PHP_MPM\MPMNotFoundException $e) {
+        \PHP_MPM\Error::save($e);
+        ob_clean();
+        header("HTTP/1.0 404 Not Found", 404, true);
+        if (ENVIRONMENT_DEV && DEBUG) {
+            $result["exception"] = print_r($e, true);
+        }
+    } catch (\PHP_MPM\MPMAuthSessionRequiredException $e) {
+        \PHP_MPM\Error::save($e);
+        ob_clean();
+        header("HTTP/1.0 403 Forbidden", 403, true);
+        if (ENVIRONMENT_DEV && DEBUG) {
+            $result["exception"] = print_r($e, true);
+        }
+    } catch (\PDOException $e) {
+        \PHP_MPM\Error::save($e);
+        ob_clean();
+        header("HTTP/1.1 500 Internal Server Error", 500, true);        
+        if (ENVIRONMENT_DEV && DEBUG) {
+            $result["exception"] = print_r($e, true);
+        }    
+    } catch (\Throwable $e) {
+        \PHP_MPM\Error::save($e);
+        ob_clean();
+        header("HTTP/1.1 500 Internal Server Error", 500, true);        
+        if (ENVIRONMENT_DEV && DEBUG) {
+            $result["exception"] = print_r($e, true);
+        }
+    } finally {
+        header("Content-Type: application/json; charset=utf-8");
+        echo json_encode($result);
+        ob_end_flush();
+    }
+?>
