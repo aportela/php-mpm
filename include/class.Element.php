99209a10d3378884998a4a32cfa1d96164947685
alex <766f6964+github@gmail.com>
2016-12-06 17:39:46 +0100

    new method for storing elements into database

--- include/class.Element.php
+++ include/class.Element.php
@@ -10,6 +10,7 @@
 
         public $id;
         public $templateId;
+        public $description;
         public $attributes;
         public $links;
         public $files;
@@ -36,6 +37,39 @@
                 $this->id = \PHP_MPM\Utils::uuid();
                 $this->attributes = $t->attributes;
                 $this->htmlForm = $t->htmlForm;
+            }
+        }
+
+        public function add() {
+            if (! \PHP_MPM\User::isAuthenticated()) {
+                throw new \PHP_MPM\MPMAuthSessionRequiredException(print_r(get_object_vars($this), true));                
+            } else if (empty($this->templateId) || empty($this->description)) {
+                throw new \PHP_MPM\MPMInvalidParamsException(print_r(get_object_vars($this), true));
+            } else {
+                $t = new \PHP_MPM\Template();
+                $t->id = $this->templateId;
+                if (! $t->allowElementCreation()) {
+                    throw new \PHP_MPM\MPMAccessDeniedException(print_r(get_object_vars($this), true));
+                } else {
+                    if (empty($this->id)) {
+                        $this->id = \PHP_MPM\Utils::uuid();
+                    }
+                    $params = array();
+                    $param = new \PHP_MPM\DatabaseParam();
+                    $param->str(":id", $this->id);
+                    $params[] = $param;                
+                    $param = new \PHP_MPM\DatabaseParam();
+                    $param->str(":template_id", $this->templateId);
+                    $params[] = $param;                
+                    $param = new \PHP_MPM\DatabaseParam();
+                    $param->str(":description", $this->description);
+                    $params[] = $param;
+                    $param = new \PHP_MPM\DatabaseParam();
+                    $param->str(":creator", User::getSessionUserId());
+                    $params[] = $param;
+                    $db = \PHP_MPM\Database::getHandler(true);
+                    $db->execWithoutResult(" INSERT INTO [ELEMENT] (id, template_id, description, created, creator) VALUES (:id, :template_id, :description, CURRENT_TIMESTAMP, :creator) ", $params);
+                }
             }
         }
     }
