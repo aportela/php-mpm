7a5fc0b2892ad870359c433ef9c5f8fe1ae3bbe1
alex <766f6964+github@gmail.com>
2018-04-21 20:47:03 +0200

    new api methods (signin/signout)

--- public/scripts/api.js
+++ public/scripts/api.js
@@ -5,4 +5,37 @@
  * all methods return callback with vue-resource response object
  */
 const phpMPMApi = {
+    /**
+     * sign in
+     */
+    signIn: function (email, password, callback) {
+        var params = {
+            user: {
+                id: null,
+                email: email,
+                password: password
+            }
+        };
+        Vue.http.post("api/user/signin", params).then(
+            response => {
+                callback(response);
+            },
+            response => {
+                callback(response);
+            }
+        );
+    },
+    /**
+     * sign out
+     */
+    signOut: function (callback) {
+        Vue.http.get("api/user/signout").then(
+            response => {
+                callback(response);
+            },
+            response => {
+                callback(response);
+            }
+        );
+    }
 };
