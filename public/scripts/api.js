7d7ed14de4181f95f0f62dcca63b1c6397e41894
alex <766f6964+github@gmail.com>
2018-04-22 18:27:27 +0200

    new user api methods (moved to user section)

--- public/scripts/api.js
+++ public/scripts/api.js
@@ -5,37 +5,57 @@
  * all methods return callback with vue-resource response object
  */
 const phpMPMApi = {
-    /**
-     * sign in
-     */
-    signIn: function (email, password, callback) {
-        var params = {
-            user: {
-                id: null,
-                email: email,
-                password: password
-            }
-        };
-        Vue.http.post("api/user/signin", params).then(
-            response => {
-                callback(response);
-            },
-            response => {
-                callback(response);
-            }
-        );
-    },
-    /**
-     * sign out
-     */
-    signOut: function (callback) {
-        Vue.http.get("api/user/signout").then(
-            response => {
-                callback(response);
-            },
-            response => {
-                callback(response);
-            }
-        );
+    user: {
+        /**
+         * sign in
+         */
+        signIn: function (email, password, callback) {
+            var params = {
+                user: {
+                    id: null,
+                    email: email,
+                    password: password
+                }
+            };
+            Vue.http.post("api/user/signin", params).then(
+                response => {
+                    callback(response);
+                },
+                response => {
+                    callback(response);
+                }
+            );
+        },
+        /**
+         * sign out
+         */
+        signOut: function (callback) {
+            Vue.http.get("api/user/signout").then(
+                response => {
+                    callback(response);
+                },
+                response => {
+                    callback(response);
+                }
+            );
+        },
+        /**
+         * search (list) users
+         */
+        search: function (name, email, actualPage, resultsPage, order, callback) {
+            var params = {
+                name: name,
+                email: email
+            };
+            Vue.http.post("api/users/", params).then(
+                response => {
+                    callback(response);
+                },
+                response => {
+                    callback(response);
+                }
+            );
+
+        }
     }
 };
