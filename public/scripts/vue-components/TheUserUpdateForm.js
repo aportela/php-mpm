0a1b5f0b00be2b5fd47402ab484a67744a89f5ff
alex <766f6964+github@gmail.com>
2018-04-23 19:44:54 +0200

    changes to have an unique user form

--- public/scripts/vue-components/TheUserUpdateForm.js
+++ public/scripts/vue-components/TheUserUpdateForm.js
@@ -5,11 +5,13 @@ const TheUserUpdateForm = (function () {
         return `
             <div class="card">
                 <div class="card-content is-clearfix">
-                    <form>
+                    <h1 class="title is-1 has-text-centered" v-if="isNew">Add new user</h1>
+                    <h1 class="title is-1 has-text-centered" v-else>Update existing user</h1>
+                    <form v-on:submit.prevent="save();">
                         <div class="field">
                             <label class="label">Full user name/surname</label>
                             <div class="control has-icons-left">
-                                <input class="input" type="text" placeholder="Type complete user name" required>
+                                <input class="input" type="text" placeholder="Type complete user name" pattern=".{3,254}" title="format: 3 to 254 characters" required v-bind:disabled="loading" v-model.trim="user.name">
                                 <span class="icon is-small is-left">
                                     <i class="fas fa-user"></i>
                                 </span>
@@ -18,7 +20,7 @@ const TheUserUpdateForm = (function () {
                         <div class="field">
                             <label class="label">Email</label>
                             <div class="control has-icons-left">
-                                <input class="input" type="email" placeholder="User email (will be used for sign in)" required>
+                            <input class="input" type="email" placeholder="User email (will be used for sign in)" pattern=".{3,254}" title="format: valid email with 3 to 254 characters" required v-bind:disabled="loading" v-model.trim="user.email">
                                 <span class="icon is-small is-left">
                                     <i class="fas fa-envelope"></i>
                                 </span>
@@ -27,7 +29,7 @@ const TheUserUpdateForm = (function () {
                         <div class="field">
                             <label class="label">Password</label>
                             <div class="control has-icons-left">
-                                <input class="input" type="password" placeholder="User password" required>
+                                <input class="input" type="password" placeholder="Type user password if you want to change" v-bind:disabled="loading" v-model.trim="user.password">
                                 <span class="icon is-small is-left">
                                     <i class="fas fa-key"></i>
                                 </span>
@@ -36,7 +38,7 @@ const TheUserUpdateForm = (function () {
                         <div class="field">
                             <label class="label">Confirm password</label>
                             <div class="control has-icons-left">
-                                <input class="input" type="password" placeholder="Type again the user password" required>
+                                <input class="input" type="password" placeholder="Type again the user password" v-bind:disabled="loading">
                                 <span class="icon is-small is-left">
                                     <i class="fas fa-key"></i>
                                 </span>
@@ -46,9 +48,9 @@ const TheUserUpdateForm = (function () {
                             <label class="label">Account type</label>
                             <div class="control">
                                 <div class="select">
-                                    <select>
-                                        <option selected>Normal user</option>
-                                        <option>Administrator</option>
+                                    <select v-model="user.accountType" v-bind:disabled="loading">
+                                        <option value="U" selected>Normal user</option>
+                                        <option value="A" >Administrator</option>
                                     </select>
                                 </div>
                             </div>
@@ -56,7 +58,7 @@ const TheUserUpdateForm = (function () {
                         <hr>
                         <div class="field is-grouped is-pulled-right">
                             <div class="control">
-                                <button class="button is-link" type="submit">
+                                <button class="button is-link" type="submit" v-bind:disabled="! allowSave">
                                     <span class="icon"><i class="fa fa-check-circle"></i></span>
                                     <label>Save changes</label>
                                 </button>
@@ -78,7 +80,72 @@ const TheUserUpdateForm = (function () {
         template: template(),
         data: function () {
             return ({
+                loading: false,
+                isNew: true,
+                user: {
+                    id: null,
+                    name: null,
+                    email: null,
+                    accountType: "U",
+                }
             });
+        },
+        created: function () {
+            if (this.$route.params.id) {
+                this.isNew = false;
+                this.get(this.$route.params.id);
+            } else {
+                this.isNew = true;
+                this.user.id = phpMPM.util.uuid();
+            }
+        },
+        computed: {
+            allowSave: function () {
+                return (this.user && this.user.id && this.user.name && this.user.email && this.user.accountType);
+            }
+        },
+        methods: {
+            get: function (id) {
+                var self = this;
+                self.loading = true;
+                phpMPMApi.user.get(id, function (response) {
+                    self.loading = false;
+                    if (response.ok) {
+                        self.user = response.body.user;
+                    } else {
+                        // TODO
+                    }
+                });
+            },
+            save: function() {
+                if (this.isNew) {
+                    this.add();
+                } else {
+                    this.update();
+                }
+            },
+            add: function () {
+                var self = this;
+                self.loading = true;
+                phpMPMApi.user.add(self.user, function (response) {
+                    self.loading = false;
+                    if (response.ok) {
+                    } else {
+                        // TODO
+                    }
+                });
+            },
+            update: function () {
+                var self = this;
+                self.loading = true;
+                phpMPMApi.user.update(self.user, function (response) {
+                    self.loading = false;
+                    if (response.ok) {
+                    } else {
+                        // TODO
+                    }
+                });
+            }
         }
     });
 
