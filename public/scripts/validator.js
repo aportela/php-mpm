8b0bdf1075fedce70ec7802fa5ca63e431627b93
alex <766f6964+github@gmail.com>
2018-04-23 21:24:27 +0200

    added new validator class

---
+++ public/scripts/validator.js
@@ -0,0 +1,31 @@
+const validator = (function () {
+    "use strict";
+
+
+    let module = {
+        invalidFields: []
+    };
+
+    module.clear = function () {
+        module.invalidFields = [];
+    }
+
+    module.setInvalid = function (field, message) {
+        module.invalidFields.push({ field: field, message: message });
+    }
+
+    module.hasInvalidField = function (field) {
+
+        return (module.invalidFields.findIndex(e => e.field == field) > -1);
+    }
+    module.getInvalidFieldMessage = function (field) {
+        let idx = module.invalidFields.findIndex(e => e.field == field);
+        if (idx > -1) {
+            return (module.invalidFields[idx].message);
+        } else {
+            return (null);
+        }
+    }
+
+    return (module);
+})();
