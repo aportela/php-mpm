1466d566f4125e5c784b085feb1967284e086b61
alex <766f6964+github@gmail.com>
2018-04-24 22:40:42 +0200

    added some icons

--- public/scripts/vue-components/TheDeleteConfirmationModal.js
+++ public/scripts/vue-components/TheDeleteConfirmationModal.js
@@ -7,15 +7,15 @@ const TheDeleteConfirmationModal = (function () {
                 <div class="modal-background"></div>
                 <div class="modal-card">
                 <header class="modal-card-head">
-                    <p class="modal-card-title">Confirmation required</p>
+                    <p class="modal-card-title"><span class="icon"><i class="fa fa-exclamation-triangle"></i></span> Confirmation required</p>
                     <button class="delete" aria-label="close" v-on:click.prevent="cancelDelete();"></button>
                 </header>
                 <section class="modal-card-body">
                     Are you sure you want to permanently remove this item ?
                 </section>
                 <footer class="modal-card-foot">
-                    <button class="button is-danger" v-on:click.prevent="confirmDelete();">Ok</button>
-                    <button class="button" v-on:click.prevent="cancelDelete();">Cancel</button>
+                    <button class="button is-danger" v-on:click.prevent="confirmDelete();"><span class="icon"><i class="fa fa-check-circle"></i></span><span>Ok</span></button>
+                    <button class="button" v-on:click.prevent="cancelDelete();"><span class="icon"><i class="fa fa-ban"></i></span><span>Cancel</span></button>
                 </footer>
                 </div>
             </div>
